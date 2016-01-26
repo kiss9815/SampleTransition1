690035bf61345eae82f8b8f0da2f0c2a68491875
kiss9815 <kiss9815@gmail.com>
2016-01-26 10:09:40 +0900

    액티비티 전환

--- app/src/main/java/com/example/tacademy/sampletransition1/OtherActivity.java
+++ app/src/main/java/com/example/tacademy/sampletransition1/OtherActivity.java
@@ -1,25 +1,23 @@
 package com.example.tacademy.sampletransition1;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class OtherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_other);
 
         Button btn = (Button)findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
-                startActivity(intent);
-                overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out);
+                finish();
+                overridePendingTransition(R.anim.slide_left_in, R.anim.slide_right_out );
             }
         });
     }
